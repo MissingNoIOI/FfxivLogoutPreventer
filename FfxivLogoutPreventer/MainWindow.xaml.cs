bd9bb12e9e8039d2fe63936bdf687b009d3ef39e
Gerri <Gerri@LAPTOP-VLF3ASCF>
2021-12-16 22:53:52 +0100

    Add project files.

---
+++ FfxivLogoutPreventer/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FfxivLogoutPreventer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource cts;
+        private bool isRunning = false;
+
+        private IKeyboardMouseEvents globalHook;
+        private Task controllerTask;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            globalHook = Hook.GlobalEvents();
+            globalHook.KeyPress += GlobalHookKeyPress;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(cts == null)
+            {
+                cts = new CancellationTokenSource();
+            }
+
+            if (isRunning)
+            {
+                cts.Cancel();
+            }
+            else
+            {
+                controllerTask = Task.Run(() => WindowController.Start(cts.Token));
+                controllerTask.GetAwaiter().OnCompleted(() =>
+                {
+                    isRunning = false;
+                    startButton.Content = "Start";
+                    startButton.Glyph = WPFUI.Common.Icon.Play12;
+                    cts?.Dispose();
+                    cts = new CancellationTokenSource();
+                });
+                
+                isRunning = true;
+                startButton.Content = "Stop";
+                startButton.Glyph = WPFUI.Common.Icon.Pause12;
+            }
+        }
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'c')
+            {
+                if (cts != null)
+                {
+                    cts.Cancel();
+                }
+            }
+        }
+    }
+}
---
+++ FfxivLogoutPreventer/WindowController.cs
@@ -0,0 +1,90 @@
+﻿using Gma.System.MouseKeyHook;
+using GregsStack.InputSimulatorStandard;
+using GregsStack.InputSimulatorStandard.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static GregsStack.InputSimulatorStandard.Native.VirtualKeyCode;
+
+namespace FfxivLogoutPreventer
+{
+    internal class WindowController
+    {
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+
+        public static async Task Start(object obj)
+        {
+            CancellationToken token = (CancellationToken)obj;
+            var FfxivWindow = FindWindow(null, "FINAL FANTASY XIV");
+            var sim = new InputSimulator();
+            var possibleKeys = new[] { VK_W, VK_A, VK_S, VK_D, SPACE, VK_E, VK_Q };
+            var r = new Random((int)DateTime.Now.Ticks); ;
+
+            var timesRun = 0;
+            var state = PRESS_STATES.NOT_PRESSED;
+            var timesToRun = 0;
+            VirtualKeyCode key = VK_W;
+
+            while (true)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    sim.Keyboard.KeyUp(key);
+                    break;
+                }
+
+
+                if (timesRun >= timesToRun)
+                {
+                    if (SetForegroundWindow(FfxivWindow))
+                    {
+                        switch (state)
+                        {
+                            case PRESS_STATES.NOT_PRESSED:
+                                key = possibleKeys[r.Next(0, 6)];
+                                sim.Keyboard.KeyDown(key);
+                                timesRun = 0;
+                                timesToRun = r.Next(5, 30);
+                                state = PRESS_STATES.PRESSED;
+                                break;
+                            case PRESS_STATES.PRESSED:
+                                sim.Keyboard.KeyUp(key);
+                                timesRun = 0;
+                                timesToRun = r.Next(10, 50);
+                                state = PRESS_STATES.WAIT;
+                                break;
+                            case PRESS_STATES.WAIT:
+                                timesRun = 0;
+                                timesToRun = 0;
+                                state = PRESS_STATES.NOT_PRESSED;
+                                break;
+                        }
+                    }
+                }
+                else
+                {
+                    timesRun++;
+                    await Task.Delay(100);
+                }
+            }
+        }
+
+        private enum PRESS_STATES
+        {
+            NOT_PRESSED,
+            PRESSED,
+            WAIT
+        }
+
+    }
+}
